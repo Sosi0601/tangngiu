56a07e8a7418935ed2e8ff39860ee4028dfee1fb
Sosi0601 <94071681+Sosi0601@users.noreply.github.com>
2022-06-07 00:54:11 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'em iu à!',
+    introDesc: `anh iu em nhiều nhất trên đời,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'iu anh <3',
+    btnNo: 'hong iu <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em rất nhiều 😘😘',
+    messDesc: 'only btram :3',
+    btnAccept: 'iuuuuuuuuuuuu',
+    messLink: 'https://www.facebook.com/profile.php?id=100008695961757'
+}
